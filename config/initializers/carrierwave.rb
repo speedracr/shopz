20579cdf55037705aac0d90888d48ee1885fa96a
speedracr <jonas@robocomobo.com>
2014-06-11 16:38:46 +0200

    Update carrierwave initializer for S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
     # :host                   => 's3.example.com',             # optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'rhopz'                            # required
+  config.fog_directory  = 'shopz'                            # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
