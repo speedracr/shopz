c27ad71c817ec7d302f417b0d38933d7bae060c0
speedracr <jonas@robocomobo.com>
2014-06-11 16:45:27 +0200

    Update fog to public

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,6 +10,6 @@ CarrierWave.configure do |config|
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'shopz'                            # required
-  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
