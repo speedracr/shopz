6b033e6f251b334897e4b649140745bacfffbad1
Your Name <you@example.com>
2014-06-12 13:45:00 +0200

    added Cart.total

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,15 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
+
+	def total
+		@total_price = 0
+		line_items.each do |item|
+			@total_price = @total_price + item.product.price_in_cents
+		end
+		@total_price
+	end
+
+	def total_in_dollars
+		total / 100
+	end
 end
