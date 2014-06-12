5f2f8375aca197bddedd73573a8332d078a70280
Your Name <you@example.com>
2014-06-12 12:09:38 +0200

    tidied set_current_cart into a concern

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,5 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-  before_action :set_current_cart
 
   def add_item_to_cart
     product_id = params[:product_id]
@@ -82,14 +81,6 @@ class CartsController < ApplicationController
     def set_cart
       @cart = Cart.find(params[:id])
     end
-
-    def set_current_cart
-      @current_cart = Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-      @current_cart = Cart.create
-      session[:cart_id] = @current_cart.id
-    end
-
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
