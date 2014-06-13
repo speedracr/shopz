b5484b2a242a6fee4f1a3c19bdf1aac46e3eabf8
Stackademy <hello@stackademy.com>
2014-06-13 09:20:32 +0200

    Replaced AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,6 @@ require 'carrierwave'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    # :aws_access_key_id      => 'AKIAJIQFP5GZLS6VHV6A',                        # required
-    # :aws_secret_access_key  => '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',                        # required
-
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],                        # required    
     :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
