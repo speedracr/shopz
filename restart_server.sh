839ceb4b9767a7c3673db754eb1bd65a6b0205e7
Stackademy <hello@stackademy.com>
2014-06-17 12:01:38 +0200

    no production

--- restart_server.sh
+++ restart_server.sh
@@ -3,5 +3,5 @@
 echo "restarting"
 kill $(cat tmp/pids/server.pid)
 bundle
-RAILS_ENV=production rake db:migrate assets:precompile
-RAILS_ENV=production nohup rails s &
+rake db:migrate assets:precompile
+nohup rails s &
